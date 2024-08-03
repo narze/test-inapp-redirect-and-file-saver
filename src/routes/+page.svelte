5e702a5f246c50b0dc82067b564f8252d0da8e6e
Manassarn Manoonchai <manassarn@gmail.com>
2024-08-03 16:17:16 +0700

    Test file saver

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script>
   import InAppSpy from "inapp-spy";
   import { on } from "svelte/events";
+  import FileSaver from "file-saver";
   const { isInApp, appKey, appName } = InAppSpy();
 
   console.log({ isInApp, appKey, appName });
+
+  function saveFile() {
+    FileSaver.saveAs("/save_me.svg", "image.svg");
+  }
+
+  function saveExternalFile() {
+    FileSaver.saveAs("https://httpbin.org/image", "image.jpg");
+  }
 </script>
 
 <h1>Check in-app browser</h1>
@@ -19,5 +28,7 @@
 <button on:click={() => window.open(window.location.href, "_blank")}
   >Open in _blank</button
 >
+<button on:click={saveFile}>Save internal image with FileSaver</button>
+<button on:click={saveExternalFile}>Save external image with FileSaver</button>
 
 <img src="https://placehold.co/600x400?text=Try Save me!" alt="" />
